8774000679b2dcf7644ea6b0595a417e1e59720d
LisReyes <lisset.rey2212@gmail.com>
2023-03-01 16:26:10 -0600

    Realizando las pruebas correspondientes

--- tests/calculator.test.js
+++ tests/calculator.test.js
@@ -3,22 +3,22 @@ const { default: expect } = require('expect');
 const calculator = require('../calculator');
 
 test('adds a + b', () => {
-    //
+    expect(calculator.sum(5,7)).toBe(12);
 })
 
 test('substracts a - b', () => {
-   //
+   expect(calculator.substract(5,3)).toBe(2);
 })
 
 test('multiplies a * b', () => {
-    //
+    expect(calculator.multiply(5,8)).toBe(40);
 })
 
 test('divides a / b', () => {
-    // Consider decimals
+    expect(calculator.divide(9,3)).toBe(3);
 })
 
 test('Raises to power a ^ b', () => {
-    //
+    expect(calculator.power(2,5)).toBe(32);
 })
 
